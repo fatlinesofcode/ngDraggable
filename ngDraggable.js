be6bc5d176beeb5b32f0e8a37b7d5c81a1715cee
Brian Bonner <brian.bonner@paraware.com>
2015-02-02 14:43:06 -0500

    Revert "- transform didn't work for me...but left did."
    This reverts commit 77d40aa0f3e30898e0058829efc3d110958863ce.

--- ngDraggable.js
+++ ngDraggable.js
@@ -220,7 +220,8 @@ angular.module("ngDraggable", [])
                     var moveElement = function (x, y) {
                             var dragContent = draggableElement(element);
                             dragContent.css({
-                            left: (x+'px'), top: (y+'px'), position: 'fixed', 'z-index': 99999
+                            transform: 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, '+x+', '+y+', 0, 1)','z-index': 99999
+                            //,margin: '0'  don't monkey with the margin,
                         });
                     }
                     initialize();
