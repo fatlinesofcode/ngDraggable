e2919a26948cd2bda92f0cb68aa6a62fa1234e03
PofMagicfingers <pof@pof.pm>
2015-08-06 17:41:15 +0200

    ng-instant-drag; draggable:will_start
    Added ng-instant-drag to prevent 100ms delay and the event draggable:will_start

--- ngDraggable.js
+++ ngDraggable.js
@@ -118,10 +118,14 @@ angular.module("ngDraggable", [])
 
                         if(_hasTouch){
                             cancelPress();
-                            _pressTimer = setTimeout(function(){
-                                cancelPress();
-                                onlongpress(evt);
-                            },100);
+                            $rootScope.$broadcast('draggable:will_start', {event:evt});
+                            _pressTimer = setTimeout(
+                                function(){
+                                    cancelPress();
+                                    onlongpress(evt);
+                                },
+                                (angular.isDefined(angular.element(evt.target).attr("ng-instant-drag")) ? 1 : 300)
+                            );
                             $document.on(_moveEvents, cancelPress);
                             $document.on(_releaseEvents, cancelPress);
                         }else{
