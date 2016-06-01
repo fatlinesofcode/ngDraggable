60fc53e0b7afeb9127cd520fa78bf704ebda2448
Alexey Matveev <alexey_matveev@epam.com>
2016-06-01 15:21:02 +0300

    Add option data-allow-move
    Credits to "anonimis" who created a [Pull Request #156](https://github.com/fatlinesofcode/ngDraggable/pull/156/files) at main ngDraggable repo on GitHub

--- ngDraggable.js
+++ ngDraggable.js
@@ -62,6 +62,7 @@ angular.module("ngDraggable", [])
           var onDragStopCallback = $parse(attrs.ngDragStop) || null;
           var onDragSuccessCallback = $parse(attrs.ngDragSuccess) || null;
           var allowTransform = angular.isDefined(attrs.allowTransform) ? scope.$eval(attrs.allowTransform) : true;
+          var allowMove = angular.isDefined(attrs.allowMove) ? scope.$eval(attrs.allowMove) : true;
 
           var getDragData = $parse(attrs.ngDragData);
 
@@ -263,6 +264,14 @@ angular.module("ngDraggable", [])
               element.css({ transform: '', 'z-index': '', '-webkit-transform': '', '-ms-transform': '' });
             else
               element.css({ 'position': '', top: '', left: '' });
+            if(allowMove) {
+                if(allowTransform) {
+                     element.css({transform:'', 'z-index':'', '-webkit-transform':'', '-ms-transform':''});
+                } 
+                else {
+                     element.css({'position':'',top:'',left:''});
+                }
+            }
           };
 
           var moveElement = function (x, y) {
@@ -276,6 +285,18 @@ angular.module("ngDraggable", [])
             } else {
               element.css({ 'left': x + 'px', 'top': y + 'px', 'position': 'fixed' });
             }
+            if(allowMove) {
+                if(allowTransform) {
+                 element.css({
+                     transform: 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, ' + x + ', ' + y + ', 0, 1)',
+                     'z-index': 99999,
+                     '-webkit-transform': 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, ' + x + ', ' + y + ', 0, 1)',
+                     '-ms-transform': 'matrix(1, 0, 0, 1, ' + x + ', ' + y + ')'
+                 });
+                }else{
+                 element.css({'left':x+'px','top':y+'px', 'position':'fixed'});
+                }
+                          }
           };
           initialize();
         }
