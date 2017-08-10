036f134b909c43c3c381ec0350c8eeb350168b56
Mikko Rekola <mikko.rekola@viasys.com>
2017-08-10 16:47:05 +0300

    Use spaces instead of tabs to indent

--- ngDraggable.js
+++ ngDraggable.js
@@ -206,13 +206,13 @@ angular.module("ngDraggable", [])
                     if(allowTransform)
                         moveElement(_tx, _ty);
                     else{
-						//Note: does not take into account _centerAnchor
-						//Click offset in element coordinates
-						var ty = _mry - _dragOffset.top;
-						var tx = _mrx - _dragOffset.left;
-						var newX = ngDraggable.inputEvent(evt).clientX - tx;
-						var newY = ngDraggable.inputEvent(evt).clientY - ty;
-						moveElement(newX, newY);
+                        //Note: does not take into account _centerAnchor
+                        //Click offset in element coordinates
+                        var ty = _mry - _dragOffset.top;
+                        var tx = _mrx - _dragOffset.left;
+                        var newX = ngDraggable.inputEvent(evt).clientX - tx;
+                        var newY = ngDraggable.inputEvent(evt).clientY - ty;
+                        moveElement(newX, newY);
                     }
 
                     $rootScope.$broadcast('draggable:move', { x: _mx, y: _my, tx: _tx, ty: _ty, event: evt, element: element, data: _data, uid: _myid, dragOffset: _dragOffset });
